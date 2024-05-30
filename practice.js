3ed1bbabf6c19d571df3cb464182116893fd802f
pchai <prempreychhay@gmail.com>
2024-05-30 16:02:23 -0700

    JS starting project

---
+++ practice.js
@@ -0,0 +1,18 @@
+// Single line comment
+
+/*
+This allow you to write multi-line comments
+I can add comments
+*/
+
+/**
+ * This is a JSdoc comment
+ * This is a test function
+ * There is nothing within it
+ * @param {string} parameter1 this is the first parameter
+ */
+function testFunction(parameter1) {}
+
+testFunction(); // Have to call a function for it to work
+
+console.log(5 != "5"); //console.log() is similar to print() in python
